ceedac5a9c34904cacc00090c5e0b0759ac743a3
Noah Hatz <Noah@Noah-Hatz-MacBook.local>
2019-11-11 18:10:55 +0100

    Paste Code in AddNumbers

--- Assets/MyGame/Scripts/AddNumbers.cs
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -7,6 +7,21 @@ public class AddNumbers : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+               int a = 3;
+
+        int b = 4;
+
+ 
+
+        Debug.Log("a = " + a);
+
+        Debug.Log("b = " + b);
+
+        Debug.Log("a + b = " + (a + b));
+
+ 
+
+        Debug.Log("add numbers a = " + a + ", b = " + b + "; a + b = " + (a + b));
         
     }
 
